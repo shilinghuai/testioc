72f444ea01b5e4731e1a59543abca5e772054bc1
shilinghuai <slhzxm16@aliyun.com>
2021-08-11 21:44:06 +0800

    z字排序，横向输出

--- src/main/java/com/test/bean/Child.java
+++ src/main/java/com/test/bean/Child.java
@@ -1,7 +1,39 @@
 package com.test.bean;
 
 public class Child extends Parent {
-//        public void sayhello2(){
-//            System.out.println("--->2");
-//        }
+    public static void main(String[] args){
+        String s = "abcdefghig";
+        int num = 3;
+        zprint(s,3);
+
+
+    }
+    public static void zprint(String s,int num){
+        StringBuffer[] result = new StringBuffer[num] ;
+        for(int ia = 0;ia<num;ia++){
+            result[ia] = new StringBuffer("");
+        }
+        result[0].append(s.charAt(0));
+        int index = 0;
+        boolean down = true;
+        for(int ib=1;ib<s.length();ib++){
+            if(down){
+                result[++index].append(s.charAt(ib));
+
+            }else {
+                result[--index].append(s.charAt(ib));
+            }
+            if(ib%(num-1)==0){
+                if(down){
+                    down = false;
+                }else {
+                    down = true;
+                }
+            }
+        }
+        for(int ic = 0;ic<num;ic++){
+            System.out.print(result[ic].toString());
+        }
+
+    }
 }
